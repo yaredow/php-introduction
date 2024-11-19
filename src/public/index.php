81b8923ed84474068f9c1f548c541d6cb9c83ae1
yaredow <yaredyilma11@gmail.com>
2024-11-19 23:23:08 +0000

    made a calculator app using basic concepts like operations and conditions

--- src/public/index.php
+++ src/public/index.php
@@ -11,27 +11,70 @@ $title = "Welcome to my PHP page!";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?php echo $title ?? 'Document'; ?></title>
+  <link rel="stylesheet" href="../css/main.css" />
 </head>
 
 <body>
-  <main>
-    <form action="../includes/formhandlers.php" method="post">
-      <label for="firstname">First Name</label>
-      <input id="firstname" type="text" name="firstname" placeholder="First name..." />
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <input type="number" name="num01" placeholder="Number one" />
+    <select name="operator">
+      <option value="add">+</option>
+      <option value="subtract">-</option>
+      <option value="multiply">*</option>
+      <option value="divide">/</option>
+    </select>
+    <input type="number" name="num02" placeholder="Number two" />
+    <button type="submit">Calculate</button>
+  </form>
 
-      <label for="lastname">Last Name</label>
-      <input id="lastname" type="text" name="lastname" placeholder="First name..." />
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Grab data from the input fields
+    $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+    $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+    $operator = htmlspecialchars($_POST["operator"]);
 
-      <label for="firstname">Favorite Pet</label>
-      <select id="favoritepet" name="favoritepet">
-        <option value="dog">Dog</option>
-        <option value="cat">Cat</option>
-        <option value="bird">Bird</option>
-      </select>
+    // Error handling
+    $error = false;
+    if (empty($num01) || empty($num02) || empty($operator)) {
+      echo "<p class='calc-error'>Fill in all fields</p>";
+      $error = true;
+    }
 
-      <button type="submit">Submit</button>
-    </form>
-  </main>
+    if (!is_numeric($num01) || !is_numeric($num02)) {
+      echo "<p class='calc-error'>Fill in all fields</p>";
+      $error = true;
+    }
+
+    //Calculate if there is no error
+    if (!$error) {
+      $value = 0;
+      switch ($operator) {
+        case 'add':
+          $value = $num01 + $num02;
+          break;
+
+        case 'subtract':
+          $value = $num01 - $num02;
+          break;
+
+        case 'multiply':
+          $value = $num01 * $num02;
+          break;
+
+        case 'divide':
+          $value = $num01 / $num02;
+          break;
+
+        default:
+          echo "<p class='calc-error'>Something went horribly wrong</p>";
+      }
+
+      echo "<p class='calc-result'>Result  = " . $value . "</p>";
+    }
+  }
+  ?>
+
 </body>
 
 </html>
